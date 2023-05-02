9fdb8df7ea5835211482b425653dd557b0a3e478
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2023-05-02 22:55:28 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 
-char board[3][3]; // игровое поле
+char board[3][3]; 
 
 void draw_board() {
-    // рисуем игровое поле
+  
     cout << "  1 2 3" << endl;
     cout << " -------" << endl;
     for (int i = 0; i < 3; i++) {
@@ -19,18 +19,18 @@ void draw_board() {
 }
 
 bool is_valid_move(int x, int y) {
-    // проверяем, можно ли сделать ход в указанную клетку
+   
     if (x < 0 || x >= 3 ||  y < 0 || y >= 3) {
-        return false; // координаты выходят за границы поля
+        return false; 
     }
     if (board[x][y] != ' ') {
-        return false; // клетка уже занята
+        return false; 
     }
     return true;
 }
 
 char check_winner() {
-    // проверяем, есть ли победитель или ничья
+
     for (int i = 0; i < 3; i++) {
         // проверяем строки
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
@@ -38,42 +38,41 @@ char check_winner() {
         }
         // проверяем столбцы
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
-            return board[0][i]; // победитель
+            return board[0][i]; 
         }
     }
     // проверяем диагонали
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
-        return board[0][0]; // победитель
+        return board[0][0]; 
     }
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
-        return board[0][2]; // победитель
+        return board[0][2]; 
     }
-    // проверяем наличие пустых клеток
+   
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (board[i][j] == ' ') {
-                return ' '; // игра продолжается
+                return ' '; 
             }
         }
     }
-    return 'D'; // ничья
+    return 'D'; 
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    // инициализируем игровое поле
+
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             board[i][j] = ' ';
         }
     }
-    // играем до победы или ничьи
+  
     char player = 'X';
     while (true) {
         draw_board();
         cout << "Ход игрока " << player << endl;
 
-        // Запросить координаты для хода
         int x, y;
         cout << "Введите номер строки: ";
         cin >> x;
@@ -82,16 +81,16 @@ int main() {
         --x;
         --y;
 
-        // Проверить, действительно ли это допустимый ход
+      
         if (!is_valid_move(x, y)) {
             cout << "Недопустимый ход! Попробуйте еще раз." << endl;
             continue;
         }
 
-        // Записать символ игрока в соответствующую ячейку на игровом поле
+     
         board[x][y] = player;
 
-        // Проверить, завершилась ли игра победой или ничьей
+      
         char winner = check_winner();
         if (winner != ' ') {
             draw_board();
