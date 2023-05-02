9859ee21bf401ff03cdc7f697fd31c2bee8e5338
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2023-05-03 00:38:28 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -32,16 +32,14 @@ bool is_valid_move(int x, int y) {
 char check_winner() {
 
     for (int i = 0; i < 3; i++) {
-        // проверяем строки
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
-            return board[i][0]; // победитель
+            return board[i][0]; 
         }
-        // проверяем столбцы
+    
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
             return board[0][i]; 
         }
     }
-    // проверяем диагонали
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
         return board[0][0]; 
     }
