435a5b69c9a890fefdf96e71fc16bbf1a535d8ef
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2023-05-09 23:07:09 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,111 +1,146 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
 
 
-char board[3][3]; 
+ char board[3][3];
 
 void draw_board() {
-  
-    cout << "  1 2 3" << endl;
-    cout << " -------" << endl;
-    for (int i = 0; i < 3; i++) {
-        cout << i + 1 << "|";
-        for (int j = 0; j < 3; j++) {
-            cout << board[i][j] << "|";
-        }
-        cout << endl;
-        cout << " -------" << endl;
-    }
+
+	cout << " 1 2 3" << "\n";
+	cout << " —-----" << "\n";
+	for (int i = 0; i < 3; i++) {
+		cout << i + 1 << "|";
+		for (int j = 0; j < 3; j++) {
+			cout << board[i][j] << "|";
+		}
+		cout << "\n";
+		cout << " —-----" << "\n";
+	}
 }
 
-bool is_valid_move(int x, int y) {
-   
-    if (x < 0 || x >= 3 ||  y < 0 || y >= 3) {
-        return false; 
-    }
-    if (board[x][y] != ' ') {
-        return false; 
-    }
-    return true;
-}
+
 
 char check_winner() {
 
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
-            return board[i][0]; 
-        }
-    
-        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
-            return board[0][i]; 
-        }
-    }
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
-        return board[0][0]; 
-    }
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
-        return board[0][2]; 
-    }
-   
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == ' ') {
-                return ' '; 
-            }
-        }
-    }
-    return 'D'; 
+	for (int i = 0; i < 3; i++) {
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+			return board[i][0];
+		}
+
+		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+			return board[0][i];
+		}
+	}
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+		return board[0][0];
+	}
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+		return board[0][2];
+	}
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (board[i][j] == ' ') {
+				return ' ';
+			}
+		}
+	}
+	return 'D';
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            board[i][j] = ' ';
-        }
-    }
-  
-    char player = 'X';
-    while (true) {
-        draw_board();
-        cout << "Ход игрока " << player << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			board[i][j] = ' ';
+		}
+	}
 
-        int x, y;
-        cout << "Введите номер строки: ";
-        cin >> x;
-        cout << "Введите номер столбца: ";
-        cin >> y;
-        --x;
-        --y;
+	char player = 'X';
+		draw_board();
+		cout << "Ход игрока " << player << "\n";
 
-      
-        if (!is_valid_move(x, y)) {
-            cout << "Недопустимый ход! Попробуйте еще раз." << endl;
-            continue;
-        }
+		int x = 0, y = 0;
+		char input;
 
-     
-        board[x][y] = player;
 
-      
-        char winner = check_winner();
-        if (winner != ' ') {
-            draw_board();
-            if (winner == 'D') {
-                cout << "Ничья!" << endl;
-            }
-            else {
-                cout << "Победил игрок " << winner << "!" << endl;
-            }
-            break;
-        }
 
-        if (player == 'X') {
-            player = 'O';
-        }
-        else {
-            player = 'X';
-        }
-    }
-}
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				cout << board[i][j] << " ";
+
+			}
+			cout << "\n";
+		}
+
+
+		while (true) {
+
+			input = _getch();
+
+			switch (input) {
+			case 'w':
+				if (y > 0) y--;
+				break;
+			case 'a':
+				if (x > 0) x--;
+				break;
+			case 's':
+				if (y < 2) y++;
+				break;
+			case 'd':
+				if (x < 2) x++;
+				break;
+			case ' ':
+				if (board[y][x] == ' ')
+				{
+					board[y][x] = player;
+
+					if (player == 'X') {
+						player = 'O';
+					}
+					else {
+						player = 'X';
+					}
+				}
+				break;
+
+			}
+
+			system("cls");
+
+
+
+			cout << " 1 2 3" << "\n";
+			cout << " —-----" << "\n";
+
+			for (int i = 0; i < 3; i++) {
+				cout << i + 1 << "|";
+				for (int j = 0; j < 3; j++) {
+					if (i == y && j == x and board[i][j] == ' ') {
+						cout << "*" << "|";
+					}
+					else cout << board[i][j] << "|";
+				}
+				cout << "\n";
+				cout << " —-----" << "\n";
+
+			}
+
+			char winner = check_winner();
+			if (winner != ' ') {
+				if (winner == 'D') {
+					cout << "Ничья!" << "\n";
+				}
+				else {
+					cout << "Победил игрок " << winner << "!" << "\n";
+				}
+				break;
+			}
+
+		}
+
+	}
